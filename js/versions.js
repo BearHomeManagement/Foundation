4801f70d269b1743a00d4d6be0b6e9cdd784bb17
Bear Home Management <robertrichardson1389@gmail.com>
2026-07-11 18:33:19 -0400

    Create versions.js

---
+++ js/versions.js
@@ -0,0 +1,10 @@
+window.BearTrackVersion = {
+    version: "1.0.0-alpha",
+    build: "2026.07.11",
+    modules: [
+        "supabase",
+        "customers",
+        "properties",
+        "workorders"
+    ]
+};
